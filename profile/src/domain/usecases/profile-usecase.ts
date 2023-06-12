89be28b3e28b90421aca1bfecf14dc95f2772731
VoN <sudo.von.contact@gmail.com>
2023-06-12 15:12:29 -0500

    feat: :sparkles: add repository

---
+++ profile/src/domain/usecases/profile-usecase.ts
@@ -0,0 +1,20 @@
+import IProfileRepository from '../repositories/profile-repository';
+import { CreateProfileEntity, ProfileEntity } from '../entities/profile-entity';
+
+interface IProfileUsecaseReader {}
+
+interface IProfileUsecaseWriter {
+  createProfile: (profilePayload: CreateProfileEntity) => Promise<ProfileEntity>;
+}
+
+interface IProfileUsecase extends IProfileUsecaseReader, IProfileUsecaseWriter {}
+
+abstract class ProfileUsecase implements IProfileUsecase {
+  constructor(
+    protected profileRepository: IProfileRepository,
+  ) {}
+
+  abstract createProfile: (profilePayload: CreateProfileEntity) => Promise<ProfileEntity>;
+}
+
+export default ProfileUsecase;
