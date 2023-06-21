22990526206c3878251020e406e1306217841590
VoN <sudo.von.contact@gmail.com>
2023-06-21 18:27:13 -0500

    feat: :sparkles: add tailwind

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.tsx",
+    "./features/**/*.tsx",
+    "./components/**/*.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
