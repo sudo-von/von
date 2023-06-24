585605c0cbc07c39d788d998db4af13ed574a978
VoN <sudo.von.contact@gmail.com>
2023-06-24 03:14:54 -0500

    fix: :bug: fix wrong rules, add tests

--- user-details/src/domain/entities/validations/profile-validations.ts
+++ user-details/src/domain/entities/validations/profile-validations.ts
@@ -29,24 +29,24 @@ export const validateName = (name: string) => {
 
 export const validateQuote = (quote: string) => {
   const quoteLength = quote.trim().length;
-  const { MIN_LENGTH, MAX_LENGTH } = profileRules.name;
+  const { MIN_LENGTH, MAX_LENGTH } = profileRules.quote;
   return quoteLength >= MIN_LENGTH && quoteLength <= MAX_LENGTH;
 };
 
 export const validatePosition = (position: string) => {
   const positionLength = position.trim().length;
-  const { MIN_LENGTH, MAX_LENGTH } = profileRules.name;
+  const { MIN_LENGTH, MAX_LENGTH } = profileRules.position;
   return positionLength >= MIN_LENGTH && positionLength <= MAX_LENGTH;
 };
 
 export const validateInterest = (interest: string) => {
   const interestLength = interest.trim().length;
-  const { MIN_LENGTH, MAX_LENGTH } = profileRules.name;
+  const { MIN_LENGTH, MAX_LENGTH } = profileRules.interest;
   return interestLength >= MIN_LENGTH && interestLength <= MAX_LENGTH;
 };
 
 export const validateProfilePicture = (profilePicture: string) => {
   const profilePictureLength = profilePicture.trim().length;
-  const { MIN_LENGTH, MAX_LENGTH } = profileRules.name;
+  const { MIN_LENGTH, MAX_LENGTH } = profileRules.profilePicture;
   return profilePictureLength >= MIN_LENGTH && profilePictureLength <= MAX_LENGTH;
 };
--- user-details/tests/domain/validations/user-validation.test.ts
+++ user-details/tests/domain/validations/user-validation.test.ts
@@ -9,33 +9,65 @@ import {
 } from '../../../src/domain/entities/validations/profile-validations';
 
 describe('profile validation', () => {
-  describe('when minimum length is validated', () => {
-    it('should return true when name is valid', () => {
+  describe('when minimum length is valid', () => {
+    it('should return true when name is validated', () => {
       const name = 'a'.repeat(profileRules.name.MIN_LENGTH);
       const isValid = validateName(name);
       expect(isValid).toBeTruthy();
     });
 
-    it('should return true when quote is valid', () => {
+    it('should return true when quote is validated', () => {
       const quote = 'a'.repeat(profileRules.quote.MIN_LENGTH);
       const isValid = validateQuote(quote);
       expect(isValid).toBeTruthy();
     });
 
-    it('should return true when position is valid', () => {
+    it('should return true when position is validated', () => {
       const position = 'a'.repeat(profileRules.position.MIN_LENGTH);
       const isValid = validatePosition(position);
       expect(isValid).toBeTruthy();
     });
 
-    it('should return true when interest is valid', () => {
+    it('should return true when interest is validated', () => {
       const interest = 'a'.repeat(profileRules.interest.MIN_LENGTH);
       const isValid = validateInterest(interest);
       expect(isValid).toBeTruthy();
     });
 
-    it('should return true when profile picture is valid', () => {
+    it('should return true when profile picture is validated', () => {
       const profilePicture = 'a'.repeat(profileRules.profilePicture.MIN_LENGTH);
+      const isValid = validateProfilePicture(profilePicture);
+      expect(isValid).toBeTruthy();
+    });
+  });
+
+  describe('when maximum length is valid', () => {
+    it('should return true when name is validated', () => {
+      const name = 'a'.repeat(profileRules.name.MAX_LENGTH);
+      const isValid = validateName(name);
+      expect(isValid).toBeTruthy();
+    });
+
+    it('should return true when quote is validated', () => {
+      const quote = 'a'.repeat(profileRules.quote.MAX_LENGTH);
+      const isValid = validateQuote(quote);
+      expect(isValid).toBeTruthy();
+    });
+
+    it('should return true when position is validated', () => {
+      const position = 'a'.repeat(profileRules.position.MAX_LENGTH);
+      const isValid = validatePosition(position);
+      expect(isValid).toBeTruthy();
+    });
+
+    it('should return true when interest is validated', () => {
+      const interest = 'a'.repeat(profileRules.interest.MAX_LENGTH);
+      const isValid = validateInterest(interest);
+      expect(isValid).toBeTruthy();
+    });
+
+    it('should return true when profile picture is validated', () => {
+      const profilePicture = 'a'.repeat(profileRules.profilePicture.MAX_LENGTH);
       const isValid = validateProfilePicture(profilePicture);
       expect(isValid).toBeTruthy();
     });
