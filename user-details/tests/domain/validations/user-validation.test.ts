9aa74685ba4c2a054d4fb84fa01576c417d022c0
VoN <sudo.von.contact@gmail.com>
2023-06-24 03:17:25 -0500

    feat: :white_check_mark: add tests

--- user-details/tests/domain/validations/user-validation.test.ts
+++ user-details/tests/domain/validations/user-validation.test.ts
@@ -72,4 +72,68 @@ describe('profile validation', () => {
       expect(isValid).toBeTruthy();
     });
   });
+
+  describe('when minimum length is invalid', () => {
+    it('should return false when name is validated', () => {
+      const name = 'a'.repeat(profileRules.name.MIN_LENGTH - 1);
+      const isValid = validateName(name);
+      expect(isValid).toBeFalsy();
+    });
+
+    it('should return false when quote is validated', () => {
+      const quote = 'a'.repeat(profileRules.quote.MIN_LENGTH - 1);
+      const isValid = validateQuote(quote);
+      expect(isValid).toBeFalsy();
+    });
+
+    it('should return false when position is validated', () => {
+      const position = 'a'.repeat(profileRules.position.MIN_LENGTH - 1);
+      const isValid = validatePosition(position);
+      expect(isValid).toBeFalsy();
+    });
+
+    it('should return false when interest is validated', () => {
+      const interest = 'a'.repeat(profileRules.interest.MIN_LENGTH - 1);
+      const isValid = validateInterest(interest);
+      expect(isValid).toBeFalsy();
+    });
+
+    it('should return false when profile picture is validated', () => {
+      const profilePicture = 'a'.repeat(profileRules.profilePicture.MIN_LENGTH - 1);
+      const isValid = validateProfilePicture(profilePicture);
+      expect(isValid).toBeFalsy();
+    });
+  });
+
+  describe('when maximum length is invalid', () => {
+    it('should return false when name is validated', () => {
+      const name = 'a'.repeat(profileRules.name.MAX_LENGTH + 1);
+      const isValid = validateName(name);
+      expect(isValid).toBeFalsy();
+    });
+
+    it('should return false when quote is validated', () => {
+      const quote = 'a'.repeat(profileRules.quote.MAX_LENGTH + 1);
+      const isValid = validateQuote(quote);
+      expect(isValid).toBeFalsy();
+    });
+
+    it('should return false when position is validated', () => {
+      const position = 'a'.repeat(profileRules.position.MAX_LENGTH + 1);
+      const isValid = validatePosition(position);
+      expect(isValid).toBeFalsy();
+    });
+
+    it('should return false when interest is validated', () => {
+      const interest = 'a'.repeat(profileRules.interest.MAX_LENGTH + 1);
+      const isValid = validateInterest(interest);
+      expect(isValid).toBeFalsy();
+    });
+
+    it('should return false when profile picture is validated', () => {
+      const profilePicture = 'a'.repeat(profileRules.profilePicture.MAX_LENGTH + 1);
+      const isValid = validateProfilePicture(profilePicture);
+      expect(isValid).toBeFalsy();
+    });
+  });
 });
