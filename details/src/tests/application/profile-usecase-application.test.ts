b0bed265f53eecfc9aea86afb88eedad5456803b
VoN <sudo.von.contact@gmail.com>
2023-06-25 21:47:57 -0500

    feat: :white_check_mark: add test

--- details/src/tests/application/profile-usecase-application.test.ts
+++ details/src/tests/application/profile-usecase-application.test.ts
@@ -3,11 +3,13 @@ import {
   expect,
   describe,
 } from '@jest/globals';
-import UserMongoRepository from '../__mocks__/user-repository-mock';
-import { ProfileEntity } from '../../domain/entities/profile-entity';
+import UserMongoRepository, { getUserByUsernameMock } from '../__mocks__/user-repository-mock';
+import { CreateProfileEntity, ProfileEntity } from '../../domain/entities/profile-entity';
 import ProfileUsecaseApplication from '../../application/profile-usecase-application';
-import ProfileMongoRepository, { getProfileByUsernameMock } from '../__mocks__/profile-repository-mock';
+import ProfileMongoRepository, { createProfileMock, getProfileByUsernameMock, getProfilesMock } from '../__mocks__/profile-repository-mock';
 import { ProfileNotFoundError } from '../../domain/errors/profile-error';
+import { UserEntity } from '../../domain/entities/user-entity';
+import { PermissionDeniedError } from '../../domain/errors/common-error';
 
 jest.mock('../../infrastructure/repositories/user-repository/mongo-repository/user-mongo-repository');
 jest.mock('../../infrastructure/repositories/profile-repository/mongo-repository/profile-mongo-repository');
@@ -23,8 +25,8 @@ describe('profileUsecase', () => {
 
   describe('getProfileByUsername', () => {
     describe('when profile exists', () => {
-      it('should return the expected profile entity by username', async () => {
-        const username = 'existing_username';
+      it('should return the expected profile by username', async () => {
+        const username = 'existing_user';
         const expectedProfileEntity: ProfileEntity = {
           id: '6498ee4d6b08e6c8a4071337',
           quote: 'This is a fake quote.',
@@ -45,12 +47,78 @@ describe('profileUsecase', () => {
 
     describe('when profile does not exist', () => {
       it('should throw an error with an appropriate message', async () => {
-        const username = 'non_existing_username';
+        const username = 'non_existing_user';
 
         getProfileByUsernameMock.mockResolvedValueOnce(null);
 
         await expect(profileUsecase.getProfileByUsername(username))
           .rejects.toThrowError(ProfileNotFoundError);
+      });
+    });
+  });
+
+  describe('createProfileByUsername', () => {
+    describe('when user has enough permissions', () => {
+      it('should return the expected created profile', async () => {
+        const requestingUsername = 'fake_user';
+        const requestedUsername = 'fake_user';
+
+        const payload: CreateProfileEntity = {
+          quote: 'This is a fake quote.',
+          interest: 'This is a fake interest.',
+          position: 'This is a fake position.',
+          username: requestedUsername,
+        };
+
+        const userEntity: UserEntity = {
+          id: '6498f976138edb6e28f62523',
+          userId: '6498ee4d6b08e6c8a4071337',
+          username: requestedUsername,
+        };
+
+        const expectedResult: ProfileEntity = {
+          id: '6498f9d771e9dc0af713b31d',
+          ...payload,
+        };
+
+        getUserByUsernameMock.mockResolvedValueOnce(userEntity);
+
+        getProfilesMock.mockResolvedValueOnce([]);
+
+        createProfileMock.mockResolvedValueOnce(expectedResult);
+
+        const result = await profileUsecase.createProfile(
+          requestingUsername,
+          requestedUsername,
+          payload,
+        );
+
+        expect(result).toEqual(expectedResult);
+
+        expect(getUserByUsernameMock).toHaveBeenCalledTimes(1);
+        expect(getProfilesMock).toHaveBeenCalledTimes(1);
+        expect(createProfileMock).toHaveBeenCalledTimes(1);
+        expect(getUserByUsernameMock).toHaveBeenCalledWith(requestedUsername);
+      });
+    });
+
+    describe('when user does not have enough permissions', () => {
+      it('should throw an error with an appropriate message', async () => {
+        const requestingUsername = 'fake_user_1';
+        const requestedUsername = 'fake_user_2';
+
+        const payload: CreateProfileEntity = {
+          quote: 'This is a fake quote.',
+          interest: 'This is a fake interest.',
+          position: 'This is a fake position.',
+          username: requestedUsername,
+        };
+
+        getProfileByUsernameMock.mockResolvedValueOnce(null);
+
+        await expect(
+          profileUsecase.createProfile(requestingUsername, requestedUsername, payload),
+        ).rejects.toThrowError(PermissionDeniedError);
       });
     });
   });
