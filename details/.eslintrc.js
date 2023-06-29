2c4712b5e86b42b237e1287d66860867b322fdba
VoN <sudo.von.contact@gmail.com>
2023-06-29 18:23:57 -0500

    build: :hammer: update scripts

--- details/.eslintrc.js
+++ details/.eslintrc.js
@@ -1,23 +1,14 @@
 module.exports = {
-  "root": true,
-  "parser": "@typescript-eslint/parser",
-  "plugins": [
-    "@typescript-eslint"
-  ],
-  "extends": [
-    "airbnb",
-    "airbnb-typescript/base"
-  ],
-  "parserOptions": {
-    "project": "./tsconfig.json",
-    "tsconfigRootDir": __dirname,
+  root: true,
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  extends: ['airbnb', 'airbnb-typescript/base'],
+  parserOptions: { project: 'tsconfig.eslint.json', sourceType: 'module' },
+  rules: {
+    'no-console': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'class-methods-use-this': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
   },
-  "rules": {
-    "no-console": "off",
-    "import/no-extraneous-dependencies": "off",
-    "class-methods-use-this": "off",
-    "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
-    "no-underscore-dangle":  ["error", { "allow": ["_id"] }]
-  },
-  "ignorePatterns": [".eslintrc.js"],
-}
+};
--- details/jest.config.js
+++ details/jest.config.js
@@ -1,7 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  verbose: true,
   preset: 'ts-jest',
   collectCoverage: true,
   testEnvironment: 'node',
+  ignorePatterns: ['tests'],
   coverageReporters: ['text'],
+  displayName: { name: 'details', color: 'green' },
 };
