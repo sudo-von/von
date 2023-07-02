dbd28d9531fc61de9694230af4b66bef84bc2039
VoN <sudo.von.contact@gmail.com>
2023-07-02 01:12:59 -0500

    build: :hammer: update scripts

--- authentication/jest.config.js
+++ authentication/jest.config.js
@@ -1,7 +1,13 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  verbose: true,
   preset: 'ts-jest',
   collectCoverage: true,
   testEnvironment: 'node',
   coverageReporters: ['text'],
+  displayName: { name: 'authentication', color: 'blue' },
+  testMatch: [
+    "<rootDir>/tests/**/*.unit-test.ts",
+    "<rootDir>/tests/**/*.integration-test.ts"
+  ],
 };
