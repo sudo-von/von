5abe3dda24e354cb943bc9d68b6d1b830bcf3aa3
VoN <sudo.von.contact@gmail.com>
2023-07-01 21:12:25 -0500

    feat: :sparkles: update profile

--- details/src/domain/entities/profile/validations/create-profile-validations.ts
+++ details/src/domain/entities/profile/validations/create-profile-validations.ts
@@ -11,20 +11,20 @@ import {
 } from '../../../errors/user-error';
 import {
   InvalidProfileQuoteLengthError,
-  InvalidProfilPositionLengthError,
+  InvalidProfilePositionLengthError,
   InvalidProfileInterestLengthError,
 } from '../profile-errors';
 import validateUsernameLength from '../../user/validations/user-validations';
 
 const validateProfileCreation = (payload: CreateProfileEntity) => {
-  const isQuoteLengthValid = validateProfileQuote(payload.quote);
-  if (!isQuoteLengthValid) throw InvalidProfileQuoteLengthError;
+  const isPositionLengthValid = validateProfilePosition(payload.position);
+  if (!isPositionLengthValid) throw InvalidProfilePositionLengthError;
 
   const isInteresLengthtValid = validateProfileInterest(payload.interest);
   if (!isInteresLengthtValid) throw InvalidProfileInterestLengthError;
 
-  const isPositionLengthValid = validateProfilePosition(payload.position);
-  if (!isPositionLengthValid) throw InvalidProfilPositionLengthError;
+  const isQuoteLengthValid = validateProfileQuote(payload.quote);
+  if (!isQuoteLengthValid) throw InvalidProfileQuoteLengthError;
 
   const isUsernameLengthValid = validateUsernameLength(payload.username);
   if (!isUsernameLengthValid) throw InvalidUsernameNameLengthError;
