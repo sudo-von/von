913db394802203725d0f958afe3d3d8a7a6d0471
VoN <sudo.von.contact@gmail.com>
2023-07-08 04:39:58 -0500

    feat: :tada: add base

---
+++ daily-ask/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  extends: ['airbnb', 'airbnb-typescript/base'],
+  parserOptions: { project: './tsconfig.eslint.json', tsconfigRootDir: __dirname },
+  rules: {
+    'no-console': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'class-methods-use-this': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+  },
+  "ignorePatterns": [".eslintrc.js", "jest.config.js"],
+};
---
+++ daily-ask/jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  verbose: true,
+  preset: 'ts-jest',
+  collectCoverage: true,
+  testEnvironment: 'node',
+  coverageReporters: ['text'],
+  displayName: { name: 'ask', color: 'yellow' },
+  testMatch: [
+    "<rootDir>/tests/**/*.test.ts",
+    "<rootDir>/tests/**/*.unit-test.ts",
+    "<rootDir>/tests/**/*.integration-test.ts"
+  ],
+};
