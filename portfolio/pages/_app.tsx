b90e101dd1577615dc1d362d09a4b3104285b152
VoN <sudo.von.contact@gmail.com>
2023-07-17 23:55:32 -0500

    feat: :sparkles: update app

--- portfolio/pages/_app.tsx
+++ portfolio/pages/_app.tsx
@@ -1,14 +1,20 @@
 import "../styles/globals.css";
 import { AppProps } from "next/app";
 import { Lexend } from "next/font/google";
+import Navbar from "../components/Navbar/Navbar";
+import Container from "../components/Container/Container";
 
 const font = Lexend({ subsets: ["latin"] });
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const className = `${font.className} mt-64`;
   return (
-    <main className={font.className}>
-      <Component {...pageProps} />
-    </main>
+    <Container>
+      <Navbar />
+      <main className={className}>
+        <Component {...pageProps} />
+      </main>
+    </Container>
   );
 };
 
