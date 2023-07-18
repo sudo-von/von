b27872b1490d0f77a926846403523e41c8f965b4
VoN <sudo.von.contact@gmail.com>
2023-07-18 14:14:39 -0500

    feat: :sparkles: add component

---
+++ portfolio/components/Button/Button.tsx
@@ -0,0 +1,22 @@
+import { ComponentPropsWithoutRef, FC } from "react";
+
+type ButtonProps = ComponentPropsWithoutRef<"button">;
+
+const Button: FC<ButtonProps> = ({ type, onClick, children, disabled }) => {
+  const backgroundClassName = "bg-black-700 hover:bg-black-850";
+  const textClassName = "text-white text-base text-center font-normal";
+  const disabledClassName = disabled ? "opacity-50 cursor-not-allowed" : "";
+  const className = `${backgroundClassName} ${textClassName} ${disabledClassName} rounded p-3`;
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      className={className}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
