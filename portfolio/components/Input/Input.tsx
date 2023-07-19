7b6216897b7b6695a02599d4143398d1761bd688
VoN <sudo.von.contact@gmail.com>
2023-07-19 17:41:44 -0500

    feat: :sparkles: add component

--- portfolio/components/Input/Input.tsx
+++ portfolio/components/Input/Input.tsx
@@ -15,7 +15,7 @@ const Input: FC<InputProps> = ({
     "text-slate-850 placeholder-slate-300 font-light text-sm";
   const borderClassName =
     "border border-slate-100 focus:border-slate-200 focus:outline-none";
-  const className = `${textClassName} ${borderClassName} rounded w-full p-3`;
+  const className = `${textClassName} ${borderClassName} rounded w-full p-3 pr-11`;
   return (
     <input
       id={id}
---
+++ portfolio/components/PasswordInput/PasswordInput.tsx
@@ -0,0 +1,42 @@
+import { FC, useState } from "react";
+import Input, { InputProps } from "../Input/Input";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+
+type PasswordInputProps = InputProps & {};
+
+const PasswordInput: FC<PasswordInputProps> = ({
+  id,
+  name,
+  value,
+  required,
+  onChange,
+  placeholder,
+}) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleVisibility = () => setIsVisible((currentState) => !currentState);
+
+  const type = isVisible ? "text" : "password";
+
+  const PasswordIcon = isVisible ? AiFillEyeInvisible : AiFillEye;
+
+  return (
+    <div className="relative">
+      <Input
+        id={id}
+        name={name}
+        type={type}
+        value={value}
+        required={required}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
+      <PasswordIcon
+        onClick={handleVisibility}
+        className="absolute top-3 right-3 cursor-pointer fill-slate-350 text-2xl"
+      />
+    </div>
+  );
+};
+
+export default PasswordInput;
