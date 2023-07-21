d362722325300a045f9c8c479677dcad662c0e54
VoN <sudo.von.contact@gmail.com>
2023-07-20 23:41:06 -0500

    feat: :sparkles: add component

--- portfolio/pages/ask/index.tsx
+++ portfolio/pages/ask/index.tsx
@@ -3,12 +3,13 @@ import Button from "../../components/Button/Button";
 import AskLayout from "../../features/ask/layouts/AskLayout/AskLayout";
 import AskProfile from "../../features/ask/components/AskProfile/AskProfile";
 import HelperInput from "../../components/HelperInput/HelperInput";
+import Typography from "../../components/Typography/Typography";
 
 const Ask: NextPage = () => {
   return (
     <AskLayout>
       <AskProfile
-        src="https://i.pinimg.com/564x/6e/7b/26/6e7b267306d1f726cc804122990ffe18.jpg"
+        src="https://i.pinimg.com/564x/e2/55/be/e255be1da7c71138cec7648fa620a0df.jpg"
         name="Jesús Rodríguez"
         position="Software engineer"
         interest="Passionate about ethical hacking"
@@ -20,6 +21,20 @@ const Ask: NextPage = () => {
       />
       <HelperInput helper="0/300" placeholder="Ask me a question!" />
       <Button>Send question</Button>
+      <div className="mt-3">
+        <div className="hover:shadow cursor-pointer flex flex-col gap-3 p-5 border border-slate-100 focus:border-slate-200 focus:outline-none rounded">
+          <Typography weight="bold" variant="caption">
+            “Lorem ipsum dolor”
+          </Typography>
+          <Typography weight="light" variant="legend" color="slate">
+            Jun 19, 2023, 3:30 PM
+          </Typography>
+          <Typography weight="light" variant="caption">
+            Lorem ipsum dolor sit amet, consectetur adipisc elit, sed do eiusmod
+            tempor incididunt ut labore. Lorem ipsum dolor sit...
+          </Typography>
+        </div>
+      </div>
     </AskLayout>
   );
 };
