4456da850fd3790eb3ccd13d0d0d453c365e3912
VoN <sudo.von.contact@gmail.com>
2023-08-21 23:22:12 -0500

    feat: :sparkles: update component

--- portfolio/features/common/components/form/components/form-footer/form-footer.tsx
+++ portfolio/features/common/components/form/components/form-footer/form-footer.tsx
@@ -1,21 +1,24 @@
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 import Hyperlink from "../../../hyperlink/hyperlink";
 import FramedBox from "../../../framed-box/framed-box";
 import FormHeading from "../form-heading/form-heading";
+import FormSubheading from "../form-subheading/form-subheading";
 
 type FormFooterProps = {
   path: string;
-  heading: string;
-  subheading: string;
+  heading: ReactNode;
+  subheading: ReactNode;
 };
 
 const FormFooter: FC<FormFooterProps> = ({ path, heading, subheading }) => {
   return (
     <>
       <FramedBox>
-        <FormHeading>{heading}</FormHeading>
+        <FormSubheading>{subheading}</FormSubheading>
       </FramedBox>
-      <Hyperlink path={path}>{subheading}</Hyperlink>
+      <Hyperlink path={path}>
+        <FormHeading>{heading}</FormHeading>
+      </Hyperlink>
     </>
   );
 };
