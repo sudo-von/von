9e731784c700c84d28cbb0e1d1b9f56ce0755624
VoN <sudo.von.contact@gmail.com>
2023-09-02 09:21:06 -0500

    feat: :sparkles: change margin

--- portfolio/flows/common/layouts/margin-layout/margin-layout.tsx
+++ portfolio/flows/common/layouts/margin-layout/margin-layout.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren } from "react";
 
 const MarginLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="mx-8 sm:mx-10 md:mx-14 lg:mx-16 xl:mx-20 2xl:mx-24 my-20">
+    <div className="mx-8 sm:mx-10 md:mx-14 lg:mx-16 xl:mx-20 my-20">
       {children}
     </div>
   );
