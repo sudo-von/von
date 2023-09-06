2775ff2b3b99b1f0a9e0b6538170266cb624bf17
VoN <sudo.von.contact@gmail.com>
2023-09-05 19:21:48 -0500

    feat: :sparkles: finish mappers

--- portfolio/flows/home/components/profile/components/details/details.mappers.ts
+++ portfolio/flows/home/components/profile/components/details/details.mappers.ts
@@ -1,9 +1,7 @@
 import { DetailsProps } from "@home/components/profile/components/details/details";
 import { UserDetailsResponse } from "@authentication/services/user-details-service/user-details.service.responses";
 
-export const toDetailsProps = (
-  detailsResponse: UserDetailsResponse
-): DetailsProps => ({
+export const toDetailsProps = (detailsResponse: UserDetailsResponse): DetailsProps => ({
   interest: detailsResponse.interest,
   position: detailsResponse.position,
   quote: detailsResponse.quote,
--- portfolio/flows/home/components/profile/components/social-network/social-network.mappers.ts
+++ portfolio/flows/home/components/profile/components/social-network/social-network.mappers.ts
@@ -1,9 +1,7 @@
 import { SocialNetworkProps } from "@home/components/profile/components/social-network/social-network";
 import { SocialNetworkResponse } from "@authentication/services/social-network-service/social-network.service.responses";
 
-export const toSocialNetworkProps = (
-  socialNetworkResponse: SocialNetworkResponse
-): SocialNetworkProps => ({
+export const toSocialNetworkProps = (socialNetworkResponse: SocialNetworkResponse): SocialNetworkProps => ({
   alt: socialNetworkResponse.name,
   href: socialNetworkResponse.url,
   src: socialNetworkResponse.src,
