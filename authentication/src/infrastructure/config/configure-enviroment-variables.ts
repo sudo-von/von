1ce869083551782915889ae1100d4725d70cab63
VoN <sudo.von.contact@gmail.com>
2023-12-17 20:59:59 -0600

    feat: :sparkles: add protocol

--- authentication/src/infrastructure/config/configure-enviroment-variables.ts
+++ authentication/src/infrastructure/config/configure-enviroment-variables.ts
@@ -15,38 +15,28 @@ const configureEnvironmentVariables = () => {
     MESSAGE_BROKER_PORT,
   } = process.env;
 
-  if (!PORT) {
-    throw new Error('PORT is not defined.');
-  }
-  if (!SECRET_KEY) {
-    throw new Error('SECRET_KEY is not defined.');
-  }
-  if (!DATABASE_NAME) {
-    throw new Error('DATABASE_NAME is not defined.');
-  }
-  if (!DATABASE_HOST) {
-    throw new Error('DATABASE_HOST is not defined.');
-  }
-  if (!DATABASE_PORT) {
-    throw new Error('DATABASE_PORT is not defined.');
-  }
-  if (!DATABASE_USERNAME) {
-    throw new Error('DATABASE_USERNAME is not defined.');
-  }
-  if (!DATABASE_PASSWORD) {
-    throw new Error('DATABASE_PASSWORD is not defined.');
-  }
-  if (!MESSAGE_BROKER_HOST) {
-    throw new Error('MESSAGE_BROKER_HOST is not defined.');
-  }
-  if (!MESSAGE_BROKER_PORT) {
-    throw new Error('MESSAGE_BROKER_PORT is not defined.');
-  }
+  if (!PORT) throw new Error('PORT is not defined.');
+
+  if (!SECRET_KEY) throw new Error('SECRET_KEY is not defined.');
+
+  if (!DATABASE_NAME) throw new Error('DATABASE_NAME is not defined.');
+
+  if (!DATABASE_HOST) throw new Error('DATABASE_HOST is not defined.');
+
+  if (!DATABASE_PORT) throw new Error('DATABASE_PORT is not defined.');
+
+  if (!DATABASE_USERNAME) throw new Error('DATABASE_USERNAME is not defined.');
+
+  if (!DATABASE_PASSWORD) throw new Error('DATABASE_PASSWORD is not defined.');
+
+  if (!MESSAGE_BROKER_HOST) throw new Error('MESSAGE_BROKER_HOST is not defined.');
+
+  if (!MESSAGE_BROKER_PORT) throw new Error('MESSAGE_BROKER_PORT is not defined.');
 
   const BASE_RADIX = 10;
   const SERVER_PORT = parseInt(PORT, BASE_RADIX);
-  const MESSAGE_BROKER_URL = `${MESSAGE_BROKER_HOST}:${MESSAGE_BROKER_PORT}`;
-  const DATABASE_URL = `${DATABASE_HOST}:${DATABASE_PORT}`;
+  const MESSAGE_BROKER_URL = `amqp://${MESSAGE_BROKER_HOST}:${MESSAGE_BROKER_PORT}`;
+  const DATABASE_URL = `mongodb://${DATABASE_HOST}:${DATABASE_PORT}`;
 
   return {
     SECRET_KEY,
