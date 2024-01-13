5992d25bfbaba3bf867915773fc0c88b3faf450a
VoN <sudo.von.contact@gmail.com>
2024-01-13 14:51:53 -0600

    feat: :sparkles: remove check

--- authentication/src/application/avatar-usecase/avatar-usecase.ts
+++ authentication/src/application/avatar-usecase/avatar-usecase.ts
@@ -23,6 +23,23 @@ import {
 import validateAvatarFileReplacement from '../../domain/entities/avatar-entity/avatar-validations/replace-avatar-file-validations';
 
 class AvatarUsecaseApplication extends AvatarUsecase {
+  deleteAvatar = async (): Promise<DetailedSecureUser> => {
+    const user = await this.userRepository.getUser();
+    if (!user) throw NoUserCreatedYetError;
+
+    if (!user.avatar) throw NoAvatarStoredYetError;
+
+    const fileExists = await this.avatarFileService.checkIfFileExists(user.avatar);
+
+    if (fileExists) await this.avatarFileService.deleteFile(user.avatar);
+
+    const updatedUser = await this.userRepository.deleteAvatar();
+    if (!updatedUser) throw AvatarDeleteFailedError;
+
+    const secureUser = detailedToSecureUser(updatedUser);
+    return secureUser;
+  };
+
   generateFilename = (hash: string, mimeType: string): string => {
     const formattedHash = hash.trim().toLowerCase();
     const formattedMimeType = mimeType.trim().toLowerCase();
@@ -35,24 +52,6 @@ class AvatarUsecaseApplication extends AvatarUsecase {
 
     const filename = `${formattedHash}.${formattedExtension}`;
     return filename;
-  };
-
-  deleteAvatar = async (): Promise<DetailedSecureUser> => {
-    const user = await this.userRepository.getUser();
-    if (!user) throw NoUserCreatedYetError;
-
-    if (!user.avatar) throw NoAvatarStoredYetError;
-
-    const fileExists = await this.avatarFileService.checkIfFileExists(user.avatar);
-    if (!fileExists) throw NoAvatarStoredYetError;
-
-    await this.avatarFileService.deleteFile(user.avatar);
-
-    const updatedUser = await this.userRepository.deleteAvatar();
-    if (!updatedUser) throw AvatarDeleteFailedError;
-
-    const secureUser = detailedToSecureUser(updatedUser);
-    return secureUser;
   };
 
   replaceAvatar = async (payload: ReplaceAvatarFile): Promise<DetailedSecureUser> => {
